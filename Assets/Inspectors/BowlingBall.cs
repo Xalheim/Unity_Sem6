6a3573602911946d9241d07522b45b71c6934404
Xal <thehinxer@gmail.com>
2024-10-03 15:16:01 +0200

    RollABallVer1
    First push, progress from start of unity tutorial to basic movement implementation

---
+++ Assets/Inspectors/BowlingBall.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowlingBall : MonoBehaviour
+{
+    public string m_Brand = "Storm";
+    public int m_ReleaseYear = 2024;
+    public Color m_Color = Color.cyan;
+    public float m_Bumpscosity = 30.23f;
+}
